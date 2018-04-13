63bdec38a96d4ae91b5d8101a52bc511792f90af
yoavmmn <yoavmmn@gmail.com>
2018-04-13 18:10:14 +0300

    added books and games

---
+++ assets/game.py
@@ -0,0 +1,43 @@
+winner = None
+current_player = 'X'
+
+board = [
+  [None, None, None],
+  [None, None, None],
+  [None, None, None]
+]
+
+def switchPlayer():
+  '''
+    This function switch the player from X to O and from O to X
+  '''
+
+def printBoard(board):
+  '''
+    This function prints the board in a nice format
+  '''
+
+def gameEnded(board):
+  '''
+    This function checks if all the spots on the board are taken
+  '''
+
+def checkForWinner(board):
+  '''
+    This function checks if there is a winner and return the winner
+  '''
+
+def getInput():
+  '''
+    This function gets an input from the players and return each players choice
+  '''
+
+def main():
+  '''
+    This function will use all the other functions
+    it should tell if the game ended in a tie or which player won
+  '''
+
+
+if __name__ == '__main__':
+  main()

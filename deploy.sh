6922bbc97d760657bc31c1a3921ef49aacf87150
yoavmmn <yoavmmn@gmail.com>
2018-04-14 04:18:00 +0300

    removed pdfs, added pdfs links

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # deploying only index.html, privacypolicy.html and assets folder
-aws s3 cp . s3://python.mmn.co.il --recursive --exclude "deploy.sh" --acl public-read --profile yoavmmn
+aws s3 cp . s3://python.mmn.co.il --recursive --exclude "deploy.sh" --exclude ".git/*" --acl public-read --profile yoavmmn

6b9964e6002fd047d6a6ba2454708100a772002c
yoavmmn <yoavmmn@gmail.com>
2018-04-14 04:09:46 +0300

    updated aws cli profile

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # deploying only index.html, privacypolicy.html and assets folder
-aws s3 cp . s3://python.mmn.co.il --recursive --exclude "deploy.sh" --acl public-read
+aws s3 cp . s3://python.mmn.co.il --recursive --exclude "deploy.sh" --acl public-read --profile yoavmmn
